0fcc939e7777b9150e8f07f1363b39eacb2d0271
Kvelu7923 <115493064+Kvelu7923@users.noreply.github.com>
2022-11-26 18:47:38 +0530

    Add files via upload

---
+++ Amazon.java
@@ -0,0 +1,26 @@
+package home;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Amazon {
+	public static void main(String[] args) {
+		
+	    WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		
+		driver.findElement(By.xpath("//a[text()='Mobiles']/following-sibling::a")).click();
+		
+		
+		
+		
+	}
+
+}
