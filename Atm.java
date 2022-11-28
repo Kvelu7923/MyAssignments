35822a063b73411714d7c7596e36f52cd28a369c
Kvelu7923 <115493064+Kvelu7923@users.noreply.github.com>
2022-11-28 08:53:00 +0530

    Add files via upload

---
+++ Atm.java
@@ -0,0 +1,51 @@
+package home;
+
+import java.util.Scanner;
+
+public class Atm {
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome To Our ATM");
+		System.out.println("Select Language");
+		System.out.println("Enter Your PIN");
+		Scanner scan=new Scanner(System.in);		
+		int a = scan.nextInt();
+		if(a==1111) {
+		
+		System.out.println("Enter Ammount");
+		}
+		
+		else {
+			System.out.println("Wrong PIN");
+			System.out.println("Try Again");
+		}
+		int c =1000;
+		int b = scan.nextInt();
+		int sum =c-b;
+	
+		
+	
+		if(b<1000) {
+			
+			System.out.println("Your Ammount is Processing");
+			System.out.println("Take Your Money");
+			System.out.println("Your Available Balance is "+ sum);	
+
+	}
+		else
+		{
+			System.out.println("Insufficient Money");
+			
+	}			
+	}	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
